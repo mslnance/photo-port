7fc4965b009b36c4fcfa0d602ca1f3a471d025da
Symphony <55611595+lnance6@users.noreply.github.com>
2021-01-11 20:21:40 -0800

    About and Nav tests

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -16,15 +16,15 @@ function Nav() {
   return (
     <header data-testid="header" className="flex-row px-1">
       <h2>
-        <a href="/">
+      <a data-testid="link" href="/">
           <span role="img" aria-label="camera"> 📸</span> Oh Snap!
         </a>
       </h2>
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a href="#about" onClick={() => handleClick()}>
-              About me
+            <a data-testid="about" href="#about" onClick={() => handleClick()}>
+               About me
             </a>
           </li>
           <li className={"mx-2"}>
